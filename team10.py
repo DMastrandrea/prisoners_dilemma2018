330b38bee295e76a1c065cd7106a8a9813abe59d
ShaneHall0 <33157441+ShaneHall0@users.noreply.github.com>
2018-12-04 14:06:46 -0500

    Add files via upload

--- team10.py
+++ team10.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Regular' # Only 10 chars displayed.
+strategy_name = 'Alternate'
+strategy_description = 'Collude, the alternate.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,7 +26,7 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    returns ('c' or 'b' for collude or betray)
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
@@ -44,6 +44,10 @@ def test_move(my_history, their_history, my_score, their_score, result):
             ") returned " + "'" + real_result + "'" +
             " and should have returned '" + result + "'")
         return False
+if len(my_history)%2 == 0:
+    return 'c'
+else:
+    return 'b'
 
 if __name__ == '__main__':
      
@@ -53,7 +57,7 @@ if __name__ == '__main__':
               my_score=0,
               their_score=0,
               result='b'):
-         print 'Test passed'
+         print ('Test passed')
      # Test 2: Continue betraying if they collude despite being betrayed.
     test_move(my_history='bbb',
               their_history='ccc', 
@@ -65,4 +69,4 @@ if __name__ == '__main__':
               # move('bbb', 'ccc', 0, 0) returns 'b'.
               my_score=0, 
               their_score=0,
-              result='b')             
+              result='b')
